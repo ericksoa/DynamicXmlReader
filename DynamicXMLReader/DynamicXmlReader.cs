f3fd884bd2050e87c64bfbb52771ceeb0a4d627c
unknown <kevint@drivensoftware.com>
2012-04-21 05:52:27 +0200

    Refactored to remove all statics

--- DynamicXMLReader/DynamicXmlReader.cs
+++ DynamicXMLReader/DynamicXmlReader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Xml.Linq;
@@ -7,138 +6,130 @@ using System.Xml.XPath;
 
 namespace DynamicXMLReader
 {
-  public class DynamicXmlReader : DynamicObject
-  {
-    private readonly XElement _xElement;
-
-    private DynamicXmlReader(XDocument xDocument)
+    public class DynamicXmlReader : DynamicObject
     {
-      _xElement = xDocument.Root;
-    }
+        private readonly XElement _xElement;
 
-    private DynamicXmlReader(XElement xElement)
-    {
-      _xElement = xElement; 
-    }
-
-    public static dynamic Parse(string someXml)
-    {
-      return new DynamicXmlReader(XDocument.Parse(someXml));
-    }
-
-    public static dynamic Load(string someUrl)
-    {
-      return new DynamicXmlReader(XDocument.Load(someUrl));
-    }
-
-    public override bool TryInvokeMember(
-      InvokeMemberBinder binder, object[] args, out object result)
-    {
-      if (binder.Name == "Using")
-      {
-        var path = (string)args[0];
-        
-        result = ArrayOfDynamicsByContext(_xElement.XPathSelectElements(path));
-        return true;
-      }
-      return base.TryInvokeMember(binder, args, out result);
-    }
-
-    private static IEnumerable<T> Append<T> (IEnumerable<T> source, T appendedItem)
-    {
-      yield return appendedItem;
-      foreach(var item in source) yield return item;
-    }
-
-    public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
-    {
-        return tryBindMultipleMatchingElements(binder, indexes[0].ToString(), out result) || base.TryGetIndex(binder, indexes, out result);
-    }
-
-    public override bool TryGetMember(GetMemberBinder binder, out object result)
-    {
-        return tryBindMultipleMatchingElements(binder, binder.Name, out result) || base.TryGetMember(binder, out result);
-    }
-
-    private bool tryBindMultipleMatchingElements(DynamicMetaObjectBinder binder, string name, out object result)
-    {
-        var matchingAttributes =
-            from attribute in _xElement.Attributes()
-            where
-                attribute.Name.LocalName.ToUpperInvariant()
-                == name.ToUpperInvariant()
-            select attribute;
-        var elements = Append(_xElement.Elements(), _xElement);
-        var matchingElements =
-            from element in elements
-            where element.Name.LocalName.ToUpperInvariant() == name.ToUpperInvariant()
-            select element;
-
-        if (tryBindToAttribute(matchingAttributes, out result)) return true;
-        if (tryBindToValue(name, out result)) return true;
-
-        if (matchingElements.Count() == 1)
-            if (binder.ReturnType != typeof(IEnumerable<object>))
-                return tryBindSingleMatchingElement(matchingElements, out result);
-            else
-                return tryBindMultipleMatchingElements(matchingElements, out result);
-        if (matchingElements.Count() > 1)
-            return tryBindMultipleMatchingElements(matchingElements, out result);
-
-        return false;
-    }
-
-    private bool tryBindMultipleMatchingElements(IEnumerable<XElement> matchingElements, out object result)
-    {
-      result = (dynamic) ArrayOfDynamicsByContext(matchingElements);
-      return true;
-    }
-
-    private bool tryBindSingleMatchingElement(IEnumerable<XElement> matchingElements, out object result)
-    {
-      var matchingElement = matchingElements.First();
-      if (matchingElement.HasElements || matchingElement.HasAttributes)
-      {
-        result = new DynamicXmlReader(matchingElement);
-        return true;
-      }
-      result = matchingElement.Value;
-      return true;
-    }
-
-    private bool tryBindToAttribute(IEnumerable<XAttribute> matchingAttributes, out object result)
-    {
-      if (matchingAttributes.Count() > 0)
-      {
-        //return single result via attribute
-        result = matchingAttributes.First().Value;
-        return true;
-      }
-      result = null;
-      return false;
-    }
-
-    private bool tryBindToValue(string name, out object result)
-    {
-        if (name == "Value")
+        internal DynamicXmlReader(XDocument xDocument)
         {
-            result = _xElement.Value;
+            _xElement = xDocument.Root;
+        }
+
+        internal DynamicXmlReader(XElement xElement)
+        {
+            _xElement = xElement;
+        }
+
+        public override bool TryInvokeMember(
+            InvokeMemberBinder binder, object[] args, out object result)
+        {
+            if (binder.Name == "Using")
+            {
+                var path = (string) args[0];
+
+                result = arrayOfDynamicsByContext(_xElement.XPathSelectElements(path));
+                return true;
+            }
+            return base.TryInvokeMember(binder, args, out result);
+        }
+
+        private IEnumerable<T> append<T>(IEnumerable<T> source, T appendedItem)
+        {
+            yield return appendedItem;
+            foreach (var item in source) yield return item;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            return tryBindMultipleMatchingElements(binder, indexes[0].ToString(), out result) ||
+                   base.TryGetIndex(binder, indexes, out result);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return tryBindMultipleMatchingElements(binder, binder.Name, out result) ||
+                   base.TryGetMember(binder, out result);
+        }
+
+        private bool tryBindMultipleMatchingElements(DynamicMetaObjectBinder binder, string name, out object result)
+        {
+            var matchingAttributes =
+                from attribute in _xElement.Attributes()
+                where
+                    attribute.Name.LocalName.ToUpperInvariant()
+                    == name.ToUpperInvariant()
+                select attribute;
+            var elements = append(_xElement.Elements(), _xElement);
+            var matchingElements =
+                from element in elements
+                where element.Name.LocalName.ToUpperInvariant() == name.ToUpperInvariant()
+                select element;
+
+            if (tryBindToAttribute(matchingAttributes, out result)) return true;
+            if (tryBindToValue(name, out result)) return true;
+
+            if (matchingElements.Count() == 1)
+                if (binder.ReturnType != typeof (IEnumerable<object>))
+                    return tryBindSingleMatchingElement(matchingElements, out result);
+                else
+                    return tryBindMultipleMatchingElements(matchingElements, out result);
+            if (matchingElements.Count() > 1)
+                return tryBindMultipleMatchingElements(matchingElements, out result);
+
+            return false;
+        }
+
+        private bool tryBindMultipleMatchingElements(IEnumerable<XElement> matchingElements, out object result)
+        {
+            result = arrayOfDynamicsByContext(matchingElements);
             return true;
         }
-        result = null;
-        return false;
-    }
 
-    private static IEnumerable<dynamic> ArrayOfDynamicsByContext(IEnumerable<XElement> elements)
-    {
-      return
-        (
-          from element in elements
-          select
-            element.HasElements || element.HasAttributes
-              ? (dynamic) new DynamicXmlReader(element)
-              : (dynamic) element.Value
-        ).ToArray();
+        private bool tryBindSingleMatchingElement(IEnumerable<XElement> matchingElements, out object result)
+        {
+            var matchingElement = matchingElements.First();
+            if (matchingElement.HasElements || matchingElement.HasAttributes)
+            {
+                result = new DynamicXmlReader(matchingElement);
+                return true;
+            }
+            result = matchingElement.Value;
+            return true;
+        }
+
+        private bool tryBindToAttribute(IEnumerable<XAttribute> matchingAttributes, out object result)
+        {
+            if (matchingAttributes.Any())
+            {
+                //return single result via attribute
+                result = matchingAttributes.First().Value;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        private bool tryBindToValue(string name, out object result)
+        {
+            if (name == "Value")
+            {
+                result = _xElement.Value;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        private IEnumerable<dynamic> arrayOfDynamicsByContext(IEnumerable<XElement> elements)
+        {
+            return
+                (
+                    from element in elements
+                    select
+                        element.HasElements || element.HasAttributes
+                            ? (dynamic) new DynamicXmlReader(element)
+                            : (dynamic) element.Value
+                ).ToArray();
+        }
     }
-  }
 }
--- DynamicXMLReaderTests/DynamicXMLReaderTests.cs
+++ DynamicXMLReaderTests/DynamicXMLReaderTests.cs
@@ -1,151 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using DynamicXMLReader;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace DynamicXMLReaderTests
 {
-  
-  [TestFixture]
-  public class DynamicXmlReaderTests
-  {
-    public void DoExpando()
+
+    [TestFixture]
+    public class DynamicXmlReaderTests
     {
-      dynamic foo = new { SomeProp =  new ExpandoObject()};
-      foo.SomeProp.MyVal = false;
+        private IDynamicXmlReaderFactory _readerFactory;
 
+        [SetUp]
+        public void Given()
+        {
+            _readerFactory = new DynamicXmlReaderFactory();
+        }
+
+        [Test]
+        public void CanReadOneLevelAttributeAsProperty()
+        {
+            const string someXml = "<name first=\"Aaron\" />";
+            var dynamicReader = _readerFactory.Parse(someXml);
+            Assert.AreEqual("Aaron", dynamicReader.Name.First);
+        }
+
+        [Test]
+        public void CanReadTwoLevelElementAsProperty()
+        {
+            const string someXml
+                = "<name first=\"Aaron\" last=\"Erickson\">" +
+                  "<age>37</age>" +
+                  "<book>The Nomadic Developer</book>" +
+                  "<book>Professional F#</book>" +
+                  "</name>";
+            var dynamicReader = _readerFactory.Parse(someXml);
+            var nomadicDev = dynamicReader.Name.Book[0];
+            Assert.AreEqual("The Nomadic Developer", nomadicDev);
+            Assert.AreEqual(37, int.Parse(dynamicReader.Name.Age));
+        }
+
+        [Test]
+        public void CanReadAListOfThingsViaXPath()
+        {
+            const string someXml
+                = "<name first=\"Aaron\" last=\"Erickson\">" +
+                  "<age>37</age>" +
+                  "<book>The Nomadic Developer</book>" +
+                  "<book>Professional F#</book>" +
+                  "<book>The Monadic Developer</book>" +
+                  "<book>Hop on Pop</book>" +
+                  "</name>";
+            var books = _readerFactory.Parse(someXml).Using("//book");
+            Assert.AreEqual("The Nomadic Developer", books[0]);
+            Assert.AreEqual("Professional F#", books[1]);
+            Assert.AreEqual("The Monadic Developer", books[2]);
+            Assert.AreEqual("Hop on Pop", books[3]);
+        }
+
+        [Test]
+        public void CanReadTheSecondBook()
+        {
+            const string someXml
+                = "<author first=\"Aaron\" last=\"Erickson\">" +
+                  "<age>37</age>" +
+                  "<book>The Nomadic Developer</book>" +
+                  "<book>Professional F#</book>" +
+                  "</author>";
+            var dynamicReader = _readerFactory.Parse(someXml);
+            Assert.AreEqual("Professional F#", dynamicReader.Author.Book[1]);
+        }
+
+        [Test]
+        public void CanReadTheFirstBooksSubTitleTitle()
+        {
+            const string someXml
+                = "<name first=\"Aaron\" last=\"Erickson\">" +
+                  "<age>37</age>" +
+                  "<book>" +
+                  "<title>The Nomadic Developer</title>" +
+                  "<subtitle>Surviving in consulting</subtitle>" +
+                  "</book>" +
+                  "<book>Professional F#</book>" +
+                  "</name>";
+            var dynamicReader = _readerFactory.Parse(someXml);
+            Assert.AreEqual("Surviving in consulting", dynamicReader.Name.Book[0].Subtitle);
+        }
+
+        [Test]
+        public void CanReadBooksUsingXPath()
+        {
+            const string someXml
+                = "<name first=\"Aaron\" last=\"Erickson\">" +
+                  "<awesomebooks>" +
+                  "<book id='Awesome'>The Big Short</book>" +
+                  "</awesomebooks>" +
+                  "<book>" +
+                  "<title>The Nomadic Developer</title>" +
+                  "<subtitle>Surviving in consulting</subtitle>" +
+                  "</book>" +
+                  "<book>Professional F#</book>" +
+                  "</name>";
+            var dynamicReader = _readerFactory.Parse(someXml).Using("//book");
+            Assert.AreEqual("Awesome", dynamicReader[0].Id);
+            Assert.AreEqual("The Big Short", dynamicReader[0].Value);
+        }
+
+        [Test]
+        public void CanReadUsingIndexer()
+        {
+            const string someXml
+                = "<name first=\"Aaron\" last=\"Erickson\">" +
+                  "<age>37</age>" +
+                  "<book>The Nomadic Developer</book>" +
+                  "<book>Professional F#</book>" +
+                  "</name>";
+            var dynamicReader = _readerFactory.Parse(someXml);
+            var result = int.Parse(dynamicReader.Name["Age"]);
+
+            Assert.That(result, Is.EqualTo(37));
+        }
+
+        //[Test]
+        //public void CanReadTccc8Rss()
+        //{
+        //  const string someXmlUrl
+        //    = "http://search.twitter.com/search.atom?q=%23tccc8";
+        //  var dynamicReader = DynamicXmlReaderDmoVersion.Load(someXmlUrl);
+        //  var entries = (IEnumerable<dynamic>) dynamicReader.Feed.Entry;
+        //  var entryCountForTccc8 = entries.Count(d => d.Title.Contains("#tccc8") || d.Title.Contains("#TCCC8"));
+        //  Assert.AreEqual(entries.Count(), entryCountForTccc8);
+        //}
     }
-    
-    [Test]
-    public void ContraVariance()
-    {
-      Action<object> someObjectAction = Console.WriteLine;
-      //someObjectAction can be used in place of something that is more specific
-      Action<string> someStringAction = someObjectAction;
-    }
-
-    public void CoVariance()
-    {
-      IEnumerable<string> someStrings = new[] {"A", "B", "C"};
-      //someStrings can be used in place of something that is more general
-      IEnumerable<object> someObjects = someStrings;
-    }
-
-    [Test]
-    public void CanReadOneLevelAttributeAsProperty()
-    {
-      const string someXml = "<name first=\"Aaron\" />";
-      var dynamicReader = DynamicXmlReader.Parse(someXml);
-      Assert.AreEqual("Aaron", dynamicReader.Name.First);
-    }
-
-    [Test]
-    public void CanReadTwoLevelElementAsProperty()
-    {
-      const string someXml 
-        = "<name first=\"Aaron\" last=\"Erickson\">" + 
-            "<age>37</age>" + 
-            "<book>The Nomadic Developer</book>" +
-            "<book>Professional F#</book>" + 
-          "</name>";
-      var dynamicReader = DynamicXmlReader.Parse(someXml);
-      var nomadicDev = dynamicReader.Name.Book[0];
-      Assert.AreEqual("The Nomadic Developer", nomadicDev);
-      Assert.AreEqual(37, int.Parse(dynamicReader.Name.Age));
-    }
-
-    [Test]
-    public void CanReadAListOfThingsViaXPath()
-    {
-      const string someXml
-        = "<name first=\"Aaron\" last=\"Erickson\">" +
-            "<age>37</age>" +
-            "<book>The Nomadic Developer</book>" +
-            "<book>Professional F#</book>" +
-            "<book>The Monadic Developer</book>" +
-            "<book>Hop on Pop</book>" +
-          "</name>";
-      var books = DynamicXmlReader.Parse(someXml).Using("//book");
-      Assert.AreEqual("The Nomadic Developer", books[0]);
-      Assert.AreEqual("Professional F#", books[1]);
-      Assert.AreEqual("The Monadic Developer", books[2]);
-      Assert.AreEqual("Hop on Pop", books[3]);
-    }
-
-    [Test]
-    public void CanReadTheSecondBook()
-    {
-      const string someXml 
-        = "<author first=\"Aaron\" last=\"Erickson\">" + 
-            "<age>37</age>" + 
-            "<book>The Nomadic Developer</book>" + 
-            "<book>Professional F#</book>" + 
-          "</author>";
-      var dynamicReader = DynamicXmlReader.Parse(someXml);
-      Assert.AreEqual("Professional F#", dynamicReader.Author.Book[1]);
-    }
-
-    [Test]
-    public void CanReadTheFirstBooksSubTitleTitle()
-    {
-      const string someXml 
-        = "<name first=\"Aaron\" last=\"Erickson\">" + 
-            "<age>37</age>" + 
-            "<book>" + 
-              "<title>The Nomadic Developer</title>" + 
-              "<subtitle>Surviving in consulting</subtitle>" + 
-            "</book>" + 
-            "<book>Professional F#</book>" + 
-          "</name>";
-      var dynamicReader = DynamicXmlReader.Parse(someXml);
-      Assert.AreEqual("Surviving in consulting", dynamicReader.Name.Book[0].Subtitle);
-    }
-
-    [Test]
-    public void CanReadBooksUsingXPath()
-    {
-      const string someXml
-        = "<name first=\"Aaron\" last=\"Erickson\">" +
-            "<awesomebooks>" + 
-              "<book id='Awesome'>The Big Short</book>" + 
-            "</awesomebooks>" +
-            "<book>" +
-              "<title>The Nomadic Developer</title>" +
-              "<subtitle>Surviving in consulting</subtitle>" +
-            "</book>" +
-            "<book>Professional F#</book>" +
-          "</name>";
-      var dynamicReader = DynamicXmlReader.Parse(someXml).Using("//book");
-      Assert.AreEqual("Awesome", dynamicReader[0].Id);
-      Assert.AreEqual("The Big Short", dynamicReader[0].Value);
-    }
-
-    [Test]
-    public void CanReadUsingIndexer()
-    {
-        const string someXml
-        = "<name first=\"Aaron\" last=\"Erickson\">" +
-            "<age>37</age>" +
-            "<book>The Nomadic Developer</book>" +
-            "<book>Professional F#</book>" +
-          "</name>";
-        var dynamicReader = DynamicXmlReader.Parse(someXml);
-        var result = int.Parse( dynamicReader.Name["Age"] );
-
-        Assert.That(result, Is.EqualTo(37));
-    }
-
-      //[Test]
-    //public void CanReadTccc8Rss()
-    //{
-    //  const string someXmlUrl
-    //    = "http://search.twitter.com/search.atom?q=%23tccc8";
-    //  var dynamicReader = DynamicXmlReaderDmoVersion.Load(someXmlUrl);
-    //  var entries = (IEnumerable<dynamic>) dynamicReader.Feed.Entry;
-    //  var entryCountForTccc8 = entries.Count(d => d.Title.Contains("#tccc8") || d.Title.Contains("#TCCC8"));
-    //  Assert.AreEqual(entries.Count(), entryCountForTccc8);
-    //}
-  }
 }
